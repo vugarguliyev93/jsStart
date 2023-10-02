7e40f2a7beb2840130beaaa37ecf34acd34846cf
vugarguliyev93 <vuqarquliyev133@gmail.com>
2023-10-02 18:07:47 +0400

    slider menu todo list

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-function PromptFunction() {
-    let text = document.getElementById("proText").innerHTML;
-
-    let age = prompt("Write age")
-    age < 18 ? alert("Get boyu") : document.getElementById("proText").innerHTML = "Boyudun";
-}
